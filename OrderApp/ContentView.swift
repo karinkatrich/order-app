52a962643e86548a14c3e4bcb51a8904c7c95c63
Karyna Katrich <karyna.katrich@mhp.com>
2021-08-10 09:40:20 +0200

    Initial Commit

---
+++ OrderApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OrderApp
+//
+//  Created by Karyna Katrich on 10.08.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OrderApp/OrderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderAppApp.swift
+//  OrderApp
+//
+//  Created by Karyna Katrich on 10.08.21.
+//
+
+import SwiftUI
+
+@main
+struct OrderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
