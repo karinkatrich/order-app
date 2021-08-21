f4300ecadb861601297ce1c5cd21af55f4e099df
Karyna Katrich <karyna.katrich@mhp.com>
2021-08-21 13:18:52 +0200

    Integrate MapKit

---
+++ OrderApp/HistoryView/MapView.swift
@@ -0,0 +1,36 @@
+//
+//  MapView.swift
+//  OrderApp
+//
+//  Created by Karyna Katrich on 21.08.21.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+
+    var latitude: Double
+    var longitude: Double
+    var regionRadius: Double
+
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        view.setRegion(MKCoordinateRegion(center: coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius),animated: true)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        view.addAnnotation(annotation)
+    }
+
+
+}
+
+//struct MapView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MapView()
+//    }
+//}
